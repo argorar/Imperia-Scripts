dd905b30dc0805656a3e78f63c83e37821626e9a
argorar <sebas199500@hotmail.com>
2018-11-07 20:58:39 -0500

    clean some code

--- ImperiaAlarm.user.js
+++ ImperiaAlarm.user.js
@@ -14,11 +14,10 @@
 
 (function() {
 
-'use strict';
-console.log("Alarma Activada");
+"use strict";
 Push.create("Información", {
     body: "Alarma activada correctamente.",
-    icon: 'https://ihcdn3.ioimg.org/iov6live/gui/favicon.png',
+    icon: "https://ihcdn3.ioimg.org/iov6live/gui/favicon.png",
     timeout: 4000,
     onClick: function () {
         window.focus();
@@ -26,12 +25,12 @@ Push.create("Información", {
     }
 });
 
-setInterval('location.reload()',600000);
+setInterval("location.reload()",600000);
 
 function addJQuery(callback) {
     var script = document.createElement("script");
     script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
-    script.addEventListener('load', function() {
+    script.addEventListener("load", function() {
         var script = document.createElement("script");
         script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
         document.body.appendChild(script);
@@ -41,15 +40,15 @@ function addJQuery(callback) {
 var estado=false;
 setInterval(
     function checker(){
-        if(document.getElementsByClassName('ui-icon attack-me')[0] != null){
-            if(estado == false){
-                document.location.href="javascript:void(xajax_viewMissions(container.open({saveName:'missions', title:'Mis misiones'}), {tab:'incoming'}))";//abre las misiones
+        if(document.getElementsByClassName("ui-icon attack-me")[0] != null){
+            if(estado === false){
+                document.location.href="javascript:void(xajax_viewMissions(container.open({saveName:"missions", title:"Mis misiones"}), {tab:"incoming"}))";//abre las misiones
                 estado=true;
-                var sound = document.createElement('object');
-                sound.setAttribute('width', '5px');
-                sound.setAttribute('height', '5px');
-                sound.setAttribute('data', 'https://freesound.org/data/previews/254/254819_4597795-lq.mp3');
-                var tdElem = document.getElementsByClassName('numeral tooltip-arrow ui-pass');//lista de ataques
+                var sound = document.createElement("object");
+                sound.setAttribute("width", "5px");
+                sound.setAttribute("height", "5px");
+                sound.setAttribute("data", "https://freesound.org/data/previews/254/254819_4597795-lq.mp3");
+                var tdElem = document.getElementsByClassName("numeral tooltip-arrow ui-pass");//lista de ataques
                 var i;
                 for (i = 0; i < tdElem.length; i++) {
                     var tdText = tdElem[i].innerText;//número del ejercito
@@ -57,20 +56,16 @@ setInterval(
                         document.body.appendChild(sound);
                         break;
                     }
-                    else{
-                        console.log("El ataque no es de peligro.");
-                    }
                 }
             }
-            sound = document.createElement('object');
-            sound.setAttribute('width', '5px');
-            sound.setAttribute('height', '5px');
-            sound.setAttribute('data', 'https://freesound.org/data/previews/254/254819_4597795-lq.mp3');
+            sound = document.createElement("object");
+            sound.setAttribute("width", "5px");
+            sound.setAttribute("height", "5px");
+            sound.setAttribute("data", "https://freesound.org/data/previews/254/254819_4597795-lq.mp3");
             document.body.appendChild(sound);
         }
-        else{
-            estado=false;
-            console.log("No hay ataques.");
+        else{//No hay ataques
+            estado=false;//se reinicia el estado
         }
 },5000);
 })();
