14fb4a11b52518b9adb48dda866c3a850c112b4b
argorar <sebas199500@hotmail.com>
2018-11-25 19:20:35 -0500

    add Sonido.js

---
+++ Sonido.js
@@ -0,0 +1,8 @@
+function start(){
+  var sound = document.createElement("object");
+  sound = document.createElement("object");
+  sound.setAttribute("width", "5px");
+  sound.setAttribute("height", "5px");
+  sound.setAttribute("data", "https://freesound.org/data/previews/254/254819_4597795-lq.mp3");
+  document.body.appendChild(sound);
+}
